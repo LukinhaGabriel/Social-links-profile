f3f614b146d560f7416e8f86e63a67d0a0b234e1
Lucas Gabriel Moya <lucasgabrielfilm@gmail.com>
2023-11-22 14:14:46 -0300

    adicionado a tag do google AdSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({ children, }:{children: React.ReactNode}){
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="keywords" content="web site, rede social" />
+                <meta name="google-adsense-account" content="ca-pub-4321324843277993" />
                 <link rel="icon" href="favicon.png" type="image/png" sizes="any"/>
                 <link rel="apple-touch-icon" href="favicon.png" type="image/png" sizes="any"/>
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet" />
